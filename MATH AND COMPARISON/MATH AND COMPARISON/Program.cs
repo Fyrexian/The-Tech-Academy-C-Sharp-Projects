94df7a50b0d977b08d176c5ed6e028f74d33bcf3
Fyrexian <vaethconner@gmail.com>
2024-05-23 21:31:43 -0500

    Math and Comparison Assginment

---
+++ MATH AND COMPARISON/MATH AND COMPARISON/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MATH_AND_COMPARISON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+
+            Console.WriteLine("Hourly Rate");
+            int P1_Rate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week");
+            int P1_Hours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Person 2");
+
+            Console.WriteLine("Hourly Rate");
+            int P2_Rate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week");
+            int P2_Hours = Convert.ToInt32(Console.ReadLine());
+
+            int P1_Annual = P1_Rate * P1_Hours * 12;
+            int P2_Annual = P2_Rate * P2_Hours * 12;
+
+            Console.WriteLine("Annual salary of Person 1:");
+            Console.WriteLine(P1_Annual);
+            Console.WriteLine("Annual salary of Person 2:");
+            Console.WriteLine(P1_Annual);
+            Console.WriteLine("Does Person 1 make more money than Person 2 ?");
+            Console.WriteLine(P1_Annual>P2_Annual);
+
+        }
+    }
+}
