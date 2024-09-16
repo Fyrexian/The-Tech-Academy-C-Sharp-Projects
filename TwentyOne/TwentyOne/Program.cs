2d288db15bbb40b56ec261b11b04535ee0aa27d8
Fyrexian <vaethconner@gmail.com>
2024-09-16 17:33:21 -0500

    Finished TwentyOne Project

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 
 namespace TwentyOne
@@ -8,16 +9,29 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-
-            foreach (Card card in deck.Cards)
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to play a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y")
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }            
-            Console.WriteLine(deck.Cards.Count);
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.IsActivelyPlaying = true;
+                while (player.IsActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
             Console.ReadLine();
+
+
         }
     }
 
--- TwentyOne/TwentyOne/obj/Debug/net8.0/TwentyOne.AssemblyInfo.cs
+++ TwentyOne/TwentyOne/obj/Debug/net8.0/TwentyOne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ee4533875cea5b83fd40bc7c1dff639ddc8398")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcfae2cdd215d149015573563470217894966a70")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwentyOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
